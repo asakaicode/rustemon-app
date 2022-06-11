30477144530eb361cbdfa1a2a7324bf39bf09342
Kaito Asahi <tubakaito5100@gmail.com>
2022-06-12 00:03:05 +0900

    delete many files

--- rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.d
+++ rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.d
@@ -1,5 +1,5 @@
-/Users/asahikaito/programming/rust/rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.rmeta: src/bin/name_to_location.rs
+/Users/asahikaito/programming/rust/rustemon-app/rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.rmeta: src/bin/name_to_location.rs
 
-/Users/asahikaito/programming/rust/rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.d: src/bin/name_to_location.rs
+/Users/asahikaito/programming/rust/rustemon-app/rustemon/target/rls/debug/deps/name_to_location-4ffe5f9d61998025.d: src/bin/name_to_location.rs
 
 src/bin/name_to_location.rs:
